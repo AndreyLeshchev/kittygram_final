291653aa91bd1c6419f76a9ba44bb41ff16a6854
Андрей Лещев <chelentano180897@yandex.ru>
2024-03-05 22:33:02 +0300

    add

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
     },
     'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     },
 }
 
